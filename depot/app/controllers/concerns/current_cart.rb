a75173a7b9d9911200ee0d8ddd46f2624d62826a
Jason Jia <ybjqx3340@gmail.com>
2017-07-01 16:54:00 -0700

    Finish D, next E

--- depot/app/controllers/concerns/current_cart.rb
+++ depot/app/controllers/concerns/current_cart.rb
@@ -5,7 +5,8 @@ module CurrentCart
   def set_cart
     @cart = Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
-    @card = Cart.create
+    puts "in rescue"
+    @cart = Cart.create
     session[:cart_id] = @cart.id
   end
 end
